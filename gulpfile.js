802e32471881618f1dd02b3f64e03dbede3ce35a
Basavaraj K N <basavarajk@Basavarajs-MacBook-Pro.local>
2017-03-01 12:20:22 +0530

    Added necessary tooling, making a intermediate commit

---
+++ gulpfile.js
@@ -0,0 +1,108 @@
+const path = require('path');
+const gulp = require('gulp');
+const usemin = require('gulp-usemin');
+const minifyHtml = require('gulp-minify-html');
+const uglify = require('gulp-uglify');
+const rev = require('gulp-rev');
+const minifyCss = require('gulp-minify-css');
+const clean = require('gulp-clean');
+const flatten = require('gulp-flatten');
+const eslint = require('gulp-eslint');
+const htmlhint = require('gulp-htmlhint');
+const mocha = require('gulp-mocha');
+
+gulp.task('usemin', ['clean'], function() {
+  return gulp.src(['webclient/*.html'])
+    .pipe(usemin({
+      html: [minifyHtml({
+        empty: true
+      })],
+      js: [uglify(), rev()],
+      inlinejs: [uglify()],
+      css: [rev()],
+      inlinecss: [minifyCss()]
+    })).pipe(gulp.dest('dist/server/public'));
+});
+
+gulp.task('copy:fonts', ['clean'], function() {
+  return gulp.src('webclient/**/*.ttf')
+    .pipe(flatten())
+    .pipe(gulp.dest('dist/server/public/fonts'));
+});
+
+gulp.task('copy:package.json', ['clean'], function() {
+  return gulp.src('package.json')
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('copy:server', ['clean'], function() {
+  gulp.src(['server/**/*'])
+    .pipe(gulp.dest('dist/server/'));
+});
+
+gulp.task('clean', function() {
+  return gulp.src('dist', {
+      read: false
+    })
+    .pipe(clean());
+});
+
+gulp.task('eslint', function() {
+  return gulp.src([
+      '!gulpfile.js', 'webpack.config.js', '.eslintrc.js',
+      'server/**/*',
+      'webclient/**/*.jsx', '!dist/**/*'
+    ])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+    .pipe(eslint.result(result => {
+      // Called for each ESLint result.
+
+      if (result.errorCount !== 0) {
+        console.log(`ESLint result: ${result.filePath}`);
+        console.log(`# Messages: ${result.messages.length}`);
+        console.log(`# Warnings: ${result.warningCount}`);
+        console.log(`# Errors: ${result.errorCount}`);
+        result.messages.forEach(function(info) {
+
+          if (info.severity === 2) {
+            console.log('\n\nLine Info       : ' + info.line + ':' +
+              info.column +
+              '\nError Message   : ' + info.message +
+              '\nSource Line     : ' + info.source +
+              '\nRule            : ' + info.ruleId + '\n\n');
+          }
+
+        })
+      }
+    }));
+});
+
+gulp.task('htmlhint', function() {
+  return gulp.src(['webclient/**/*.html', '!node_modules/**/*',
+      '!bower_components/**/*', '!dist/**/*'
+    ])
+    .pipe(htmlhint({
+      htmlhintrc: '.htmlhintrc'
+    }))
+    .pipe(htmlhint.failReporter());
+});
+
+gulp.task('test', function() {
+  return gulp.src(['test/**/*spec.js', 'test/**/*test.js', 'test/*spec.js',
+      'test/**/*test.js', '!node_modules/**/*',
+      '!bower_components/**/*'
+    ], {
+      read: false
+    })
+    .pipe(mocha());
+});
+
+gulp.task('copy', ['copy:package.json', 'copy:server', 'copy:fonts']);
+
+gulp.task('build', ['eslint', 'usemin', 'copy']);
+
+gulp.task('lint', ['eslint', 'htmlhint']);
+
+gulp.task('default', ['build']);
