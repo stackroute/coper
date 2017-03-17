b6763e9a7366c2b8633ca5603345ebdf682a8110
prempr7 <prempr7@gmail.com>
2017-03-17 12:32:59 +0530

    mongodb url changed from online to local

--- server/services/config/database.js
+++ server/services/config/database.js
@@ -1,4 +1,3 @@
 module.exports = {
-//    url: 'mongodb://localhost:27017/lucy'
-url: 'mongodb://prempr7:qwerty12345@olympia.modulusmongo.net:27017/ahU6hiqo'
+    url: 'mongodb://localhost:27017/lucy'
 };
