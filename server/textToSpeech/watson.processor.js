12e4a915d4347a6a7b6b926c6107d1c7df1582d2
sakshi3195 <sakshimehta3195@gmail.com>
2017-03-29 14:33:44 +0530

    TextToSpeech  module added config folder modified

---
+++ server/textToSpeech/watson.processor.js
@@ -0,0 +1,17 @@
+
+var textToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
+var watsonConfig = require('../../config/textToSpeech.js');
+var fs = require('fs');
+//This function is to convert the response text to speech.
+var text_to_speech = new textToSpeechV1(watsonConfig);
+
+var params = {
+  text: 'hii i aa prem prakash',
+  voice: 'en-US_AllisonVoice',
+  accept: 'audio/wav'
+};
+
+
+//text_to_speech.synthesize(params).pipe(fs.createWriteStream('output7.wav'));
+
+module.exports = text_to_speech;
