00bd449bc01e4987ac706009164c27c3533ed355
Basavaraj K N <basavarajk@Basavarajs-MacBook-Pro.local>
2017-04-04 12:58:44 +0530

    adding kafka based consumer pipeline base modules, intermediate commit

--- services/service.base.js
+++ services/service.base.js
@@ -2,7 +2,13 @@ const highland = require('highland');
 const kafka = require('kafka-node');
 const config = require('../config/config');
 
-// A consumer needs to specify
+/*
+ * This is a KAFKA Consumer base service module, which subscribes to Kafka topic perpetually
+ *
+ * This module can be used as a base module method, wherever kafka topic subscription is needed
+ */
+
+// A client (calee) needs to specify
 // - Topic name it wants to subscribe to
 // - Consumer group it belongs to
 // - Kafka host it wants to connect to
@@ -32,7 +38,12 @@ function run(subscribeTopic, consumerGroup, kafkaHost, processPipeLine) {
       consumer.on('message', function(message) {
         console.log('Message received: ', message);
 
-        push(null, dataObj);
+        //If message is not JSON, parse it as JSON here, before passing it to the rest of the pipeline
+
+        //Push the message from kafka to rest of the pipeline
+        push(null, message);
+
+        //Start calling the generator again for listening to next message
         next();
       });
 
@@ -43,14 +54,22 @@ function run(subscribeTopic, consumerGroup, kafkaHost, processPipeLine) {
         next();
       });
     }).map(function(messageObj) {
+      //Temporarily keeping this map method, to intermediary log and verify if messages are coming from Kafka or not
+      //Once well tested, this method can be removed
       console.log('Received a message: ', messageObj);
-    })
-    .pipe(processPipeLine)
-    .errors(function(err) {
-      console.log('Got errors: ', err);
-    })
-    .each(function(messageObj) {});
 
+      //If not returned, the message will not be propagated to next set of pipeline
+      return messageObj;
+    })
+    .pipe(processPipeLine) //Assemble the calee's processing pipeline
+    .errors(function(err) {
+      //Listen to any error if happens
+      console.log('Got errors: ', err);
+      return err;
+    })
+    .each(function(messageObj) {
+      //Drain the message from pipeline, so that next message can enter the pipeline
+    });
 }
 
 module.exports = {
