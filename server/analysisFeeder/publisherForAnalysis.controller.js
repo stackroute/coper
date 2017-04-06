b4ff021cfc19191628290f8caddf2e6d24c72efb
Basavaraj K N <basavarajk@Basavarajs-MacBook-Pro.local>
2017-04-06 11:12:21 +0530

    Fixed few changes to handle the publishing

--- server/analysisFeeder/publisherForAnalysis.controller.js
+++ server/analysisFeeder/publisherForAnalysis.controller.js
@@ -4,18 +4,16 @@ const async = require('async');
 const log4js = require('log4js');
 log4js.loadAppender('console');
 
-const publishToAnalyze = function(analysisTopic, dataToPublish, callback) {
-  console.log('Request recieved to publish to ', analysisTopic);
 
-  publishToKafkaTopic(analysisTopic, dataToPublish, function(err, publishResult) {
-    console.log('Done publishing, with result ', publishResult);
-    callback(err, publishResult);
-  });
+const logger = log4js.getLogger('AnalysisFeeder');
+
+const publishToAnalyze = function(analysisTopic, dataToPublish, callback) {
+  logger.debug('Request received to publish to ', analysisTopic);
+
+  publishToKafkaTopic(analysisTopic, dataToPublish, callback);
 }
 
-const publishToKafkaTopic = function(topicName, dataPayload, callback) {
-  let client = new kafka.Client(config.ZOOKEEPER.URL);
-
+const publishToKafkaTopic = function(topicName, dataPayload, publishCallback) {
   async.waterfall([
       function(callback) {
         upsertKafkaTopic(topicName, callback)
@@ -25,24 +23,42 @@ const publishToKafkaTopic = function(topicName, dataPayload, callback) {
       }
     ],
     function(err, publishResults) {
-      console.log('Finished publishToKafkaTopic with ', ' err: ', err, ' result: ', publishResults);
-      callback(err, publishResults)
+      logger.debug('Finished publishToKafkaTopic with ', ' err: ', err,
+        ' result: ', publishResults);
+      publishCallback(err, publishResults);
     });
 }
 
 const upsertKafkaTopic = function(topicName, callback) {
-  let client = new kafka.Client(config.ZOOKEEPER.URL);
+  callback(null, 'good');
+  return;
+  try {
+    logger.debug('Connecting to ', config.ZOOKEEPER.URL);
+    let client = new kafka.Client(config.ZOOKEEPER.URL);
 
-  client.once('connect', function() {
-    client.loadMetadataForTopics([topicName], (err, resp) => {
-      console.log("Topic upsert result:", JSON.stringify(resp));
-      callback(err, resp);
+    client.once('connect', function(err) {
+      if (err) {
+        logger.error('Error in connecting for upsertKafkaTopic', err);
+        callback(err, null);
+        return;
+      }
+
+      client.loadMetadataForTopics([topicName], (err, resp) => {
+        logger.debug("Topic upsert finish with err: ", err,
+          " result: ",
+          JSON.stringify(resp));
+
+        callback(err, resp);
+      });
     });
-  });
+  } catch (err) {
+    logger.error('unexpected error in upsertKafkaTopic ', err);
+    callback(err, null);
+  }
 }
 
 const publishToKafka = function(topicName, dataPayload, callback) {
-  console.log('Publishing to topic ', topicName, ' with data: ', dataPayload);
+  logger.debug('Publishing to topic ', topicName, ' with data: ', dataPayload);
 
   let client = new kafka.Client(config.ZOOKEEPER.URL);
   let producer = new kafka.Producer(client);
@@ -60,7 +76,8 @@ const publishToKafka = function(topicName, dataPayload, callback) {
         callback(err, null)
         return;
       }
-      console.log('Published message to messaging pipeline topic ', topicName, ' with result: ', data);
+      logger.debug('Published message to messaging pipeline topic ',
+        topicName, ' with result: ', data);
       callback(null, data);
       return;
     });
--- server/websockets/websocket.server.js
+++ server/websockets/websocket.server.js
@@ -17,84 +17,91 @@ const redisClient = redis.createClient(redisConfig);
 
 const wsService = function(server) {
 
-    const wsServer = socketIO(server);
+  const wsServer = socketIO(server);
 
-    wsServer.on('connection', function(clientSocket) {
-        console.log('[*] Got new client socket connection ');
-        // PING is the event name
-        clientSocket.on('PING', function(message) {
-            console.log('received: %s', message);
+  wsServer.on('connection', function(clientSocket) {
+    console.log('[*] Got new client socket connection ');
+    // PING is the event name
+    clientSocket.on('PING', function(message) {
+      console.log('received: %s', message);
 
-            let now = new Date();
+      let now = new Date();
 
-            // PONG is the event name
-            clientSocket.emit('PONG', 'pong ping');
-        });
-        let username = '';
-        clientSocket.on('send::userToken',function(uToken){
-          userToken = authController.authenticatePage(uToken).then(function(user) {
-            username = user.username;
-            redisClient.subscribe('conversation::new::'+user.username);
-          }, function(err) {
-              logger.error(err);
-          });
-
-        })
-
-        ss(clientSocket).on('stream::speech', function(stream) {
-            stream.pipe(speechToTextProcessor.getSpeechRecognizeStream()).on('error', function(err) {
-                //@TODO handle the error and communicate to client socket appropriately
-                if (err) {
-                    console.log(err);
-                }
-            }).on('data', function(data) {
-                logger.debug(data.results);
-
-                //@TODO On uttrance is complete, send the text to detect the intent and further processing
-
-                if (data.endpointerType === 'ENDPOINTER_EVENT_UNSPECIFIED') {
-                    clientSocket.emit('send::text', data.results);
-                }
-            }).on('close', function() {
-                logger.debug('pipe close');
-            }).on('end', function() {
-                logger.debug('pipe end');
-            });
-            stream.on('data', function(data) {
-                //logger.debug(data);
-            });
-            stream.on('end', function() {
-                logger.debug('streaming end');
-            });
-        });
-        clientSocket.on('utterance::new', function(message) {
-            logger.debug('utterance : ', message);
-            utteranceReceiver.processUtterance(username,message.conversation.startTime,message.utterance);
-        });
-        clientSocket.on('disconnect', function() {
-            logger.debug('[*] Client socket disconnected ...!');
+      // PONG is the event name
+      clientSocket.emit('PONG', 'pong ping');
+    });
+    let username = '';
+    clientSocket.on('send::userToken', function(uToken) {
+      userToken = authController.authenticatePage(uToken).then(
+        function(user) {
+          username = user.username;
+          redisClient.subscribe('conversation::new::' + user.username);
+        },
+        function(err) {
+          logger.error(err);
         });
 
-        redisClient.on('ready', function(){
-          logger.debug('ready');
+    })
 
-        });
-        const speechStream = ss.createStream();
-        ss(clientSocket).emit('stream::textToSpeech',speechStream);
-        redisClient.on('message',function(channel,message) {
-          logger.debug(message);
-          if(channel === 'conversation::new::'+username)
-          {
-            logger.debug(message);
-            clientSocket.emit('conversation::start',JSON.parse(message));
+    ss(clientSocket).on('stream::speech', function(stream) {
+      stream.pipe(speechToTextProcessor.getSpeechRecognizeStream()).on(
+        'error',
+        function(err) {
+          //@TODO handle the error and communicate to client socket appropriately
+          if (err) {
+            console.log(err);
           }
-          else if(channel === 'conversation::received::'+username)
-          {
-            logger.debug('conversation::received::');
-            clientSocket.emit('utterance::received',JSON.parse(message));
-          }
+        }).on('data', function(data) {
+        logger.debug(data.results);
+
+        //@TODO On uttrance is complete, send the text to detect the intent and further processing
+
+        if (data.endpointerType ===
+          'ENDPOINTER_EVENT_UNSPECIFIED') {
+          clientSocket.emit('send::text', data.results);
+        }
+      }).on('close', function() {
+        logger.debug('pipe close');
+      }).on('end', function() {
+        logger.debug('pipe end');
+      });
+      stream.on('data', function(data) {
+        //logger.debug(data);
+      });
+      stream.on('end', function() {
+        logger.debug('streaming end');
+      });
+    });
+    clientSocket.on('utterance::new', function(message) {
+      logger.debug('utterance : ', message);
+      utteranceReceiver.processUtterance(username, message.conversation
+        .startTime, message.utterance,
+        function(newConvObj, utteranceText) {
+          logger.debug('Conversation for [', utteranceText,
+            '] registered ', newConvObj, utteranceText);
         });
     });
+    clientSocket.on('disconnect', function() {
+      logger.debug('[*] Client socket disconnected ...!');
+    });
+
+    redisClient.on('ready', function() {
+      logger.debug('ready');
+
+    });
+    const speechStream = ss.createStream();
+    ss(clientSocket).emit('stream::textToSpeech', speechStream);
+    redisClient.on('message', function(channel, message) {
+      logger.debug(message);
+      if (channel === 'conversation::new::' + username) {
+        logger.debug(message);
+        clientSocket.emit('conversation::start', JSON.parse(message));
+      } else if (channel === 'conversation::received::' + username) {
+        logger.debug('conversation::received::');
+        clientSocket.emit('utterance::received', JSON.parse(message));
+      }
+    });
+  });
 }
 
 module.exports = wsService;
