f32f84370a19eaa1096e81f3396061fb20a27105
souravDutta123 <sourav08.20@gmail.com>
2017-04-06 20:30:53 +0530

    error correction in websocket

--- server/websockets/websocket.server.js
+++ server/websockets/websocket.server.js
@@ -1,83 +1,67 @@
 const socketIO = require('socket.io');
 const ss = require('socket.io-stream');
-
 const redis = require('redis');
-
 const log4js = require('log4js');
 log4js.loadAppender('console');
 //log4js.addAppender(log4js.appenders.file('./logs/binaryjs.log'), 'binaryServer');
 const logger = log4js.getLogger('wsService');
-
 const utteranceReceiver = require('../utteranceReceiver');
 const speechToTextProcessor = require('../speechToText');
 const textToSpeech = require('../textToSpeech');
 const authController = require('../authentication/authentication.controller');
 const config = require('../../config/config').REDIS_CLIENT;
-
 const redisClient = redis.createClient(config.REDIS_CLIENT);
-
 const wsService = function(server) {
-
   const wsServer = socketIO(server);
-
   wsServer.on('connection', function(clientSocket) {
     console.log('[*] Got new client socket connection ');
     // PING is the event name
     clientSocket.on('PING', function(message) {
       console.log('received: %s', message);
-
       let now = new Date();
-            // PONG is the event name
-            clientSocket.emit('PONG', 'pong ping');
+      // PONG is the event name
+      clientSocket.emit('PONG', 'pong ping');
+    });
+    let username = '';
+    clientSocket.on('send::userToken', function(uToken) {
+      userToken = authController.authenticatePage(uToken).then(
+        function(user) {
+          username = user.username;
+          redisClient.subscribe('conversation::new::' + user.username);
+          redisClient.subscribe('utterance::received::' + username);
+          redisClient.subscribe('conversation::response::' + username);
+        },
+        function(err) {
+          logger.error(err);
         });
-        let username = '';
-        clientSocket.on('send::userToken',function(uToken){
-          userToken = authController.authenticatePage(uToken).then(function(user) {
-            username = user.username;
-            redisClient.subscribe('conversation::new::'+user.username);
-            redisClient.subscribe('utterance::received::'+user.username);
-            redisClient.subscribe('conversation::response::'+user.username);
-          }, function(err) {
-              logger.error(err);
-          });
-
-        })
-
-        ss(clientSocket).on('stream::speech', function(stream) {
-            stream.pipe(speechToTextProcessor.getSpeechRecognizeStream()).on('error', function(err) {
-                //@TODO handle the error and communicate to client socket appropriately
-                if (err) {
-                    console.log(err);
-                }
-            }).on('data', function(data) {
-                logger.debug(data.results);
-
-                //@TODO On uttrance is complete, send the text to detect the intent and further processing
-
-                if (data.endpointerType === 'ENDPOINTER_EVENT_UNSPECIFIED') {
-                    clientSocket.emit('send::text', data.results);
-                }
-            }).on('close', function() {
-                logger.debug('pipe close');
-            }).on('end', function() {
-                logger.debug('pipe end');
-            });
-            stream.on('data', function(data) {
-                //logger.debug(data);
-            });
-            stream.on('end', function() {
-                logger.debug('streaming end');
-            });
-        });
-        clientSocket.on('utterance::new', function(message) {
-            logger.debug('utterance : ', message);
-            utteranceReceiver.processUtterance(username,message.conversation.startTime,message.utterance);
-        });
-        clientSocket.on('disconnect', function() {
-            logger.debug('[*] Client socket disconnected ...!');
-        });
-
-
+    })
+    ss(clientSocket).on('stream::speech', function(stream) {
+      stream.pipe(speechToTextProcessor.getSpeechRecognizeStream()).on(
+        'error',
+        function(err) {
+          //@TODO handle the error and communicate to client socket appropriately
+          if (err) {
+            console.log(err);
+          }
+        }).on('data', function(data) {
+        logger.debug(data.results);
+        //@TODO On uttrance is complete, send the text to detect the intent and further processing
+        if (data.endpointerType ===
+          'ENDPOINTER_EVENT_UNSPECIFIED') {
+          clientSocket.emit('send::text', data.results);
+        }
+      }).on('close', function() {
+        logger.debug('pipe close');
+      }).on('end', function() {
+        logger.debug('pipe end');
+      });
+      stream.on('data', function(data) {
+        //logger.debug(data);
+      });
+      stream.on('end', function() {
+        logger.debug('streaming end');
+      });
+    });
     clientSocket.on('utterance::new', function(message) {
       logger.debug('utterance : ', message);
       utteranceReceiver.processUtterance(username, message.conversation
@@ -90,15 +74,11 @@ const wsService = function(server) {
     clientSocket.on('disconnect', function() {
       logger.debug('[*] Client socket disconnected ...!');
     });
-
     redisClient.on('ready', function() {
       logger.debug('ready');
-
     });
-
     const speechStream = ss.createStream();
     ss(clientSocket).emit('stream::textToSpeech', speechStream);
-
     redisClient.on('message', function(channel, message) {
       logger.debug('channel',channel);
       if (channel === 'conversation::new::' + username) {
@@ -114,5 +94,4 @@ const wsService = function(server) {
     });
   });
 }
-
 module.exports = wsService;
